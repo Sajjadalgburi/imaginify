0acdbb7eed8d3fe699c882fe2c70259d1b3db241
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-05 14:13:11 -0400

    feat: Add UserButton component for sign-out functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <p>home</p>
+
+      <UserButton afterSignOutUrl="/" />
+    </>
+  );
 };
 
 export default Home;
