6f70e344db74f98aa09ef4c0b82041f35f24c87b
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-06 14:59:07 -0400

    feat: Add ImageInterface for storing image data in the database

---
+++ interfaces/int.ts
@@ -0,0 +1,23 @@
+import { Document } from "mongoose";
+
+// Define the interface for the Image document
+export interface ImageInterface extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: URL;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: URL;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
