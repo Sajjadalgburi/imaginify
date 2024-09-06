bd8168122cfbf3603381fc61a5818c5fa8cc5bbb
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-06 15:09:15 -0400

    feat: Add User model for storing user data in the database

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+import { IUser } from "@/interfaces/int";
+
+const UserSchema = new Schema<IUser>({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
+
+const User = models?.User || model<IUser>("User", UserSchema);
+
+export default User;
