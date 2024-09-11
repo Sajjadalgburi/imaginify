05f0b28f0ece9a4b0a09b80cb62e85bc32ea6a87
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-11 09:37:01 -0400

    Refactor AddTransformationType component to include Header and TForm components

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+/* eslint-disable @next/next/no-async-client-component */
+"use client";
 
-const AddTransformationType = () => {
-  return <div>AddTransformationType</div>;
+import Header from "@/components/shared/Header";
+import { TForm } from "@/components/shared/TransformationFrom";
+import { transformationTypes } from "@/constants";
+import React from "react";
+import { getUserById } from "@/lib/actions/user.actions";
+
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const AddTransformationType = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transfromation = transformationTypes[type];
+
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId as string);
+
+  return (
+    <>
+      <Header title={transfromation.title} subtitle={transfromation.subTitle} />
+
+      <TForm
+        action="Add"
+        userId={user._id}
+        type={transfromation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
 };
 
 export default AddTransformationType;
